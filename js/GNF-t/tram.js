119961b66d621146864b201ec1b0935db71ed5f8
Zhangpengyu <18746442232@163.com>
2021-05-20 19:29:31 +0800

    还就那个柯里化

--- js/GNF-t/tram.js
+++ js/GNF-t/tram.js
@@ -1,5 +1,11 @@
 const curry = (fn, ...args) => args.length >= fn.length ? fn(...args) : (..._args) => curry(fn, ...args, ..._args)
-const curry2 = (fn, ...args) =>{ args.length >= fn.length ? fn(...args) : (..._args) => curry(fn, ...args, ..._args)}
+const curry2 = (fn, ...args) => {
+    return (args.length >= fn.length ? fn(...args) : (..._args) => {
+        console.log('...args', ...args, '..._args:', ..._args);
+        return curry(fn, ...args, ..._args)
+    })
+}
+
 function fun(x, y, z) {
     return x + y + z;
 }
@@ -7,3 +13,10 @@ const add = curry(fun);
 const add2 = curry2(fun);
 console.log(add(1, 2, 3));
 console.log(add2(1, 2, 3));
+(make_fact => 
+    make_fact(make_fact)
+    )
+    (make_fact => 
+        (fact => n => n === 0 ? 0 : n + fact(n - 1))
+        (x => make_fact(make_fact)(x)))
+        (15)
