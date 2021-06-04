41b4bbcd63ee3a6eaf3c0383c1a03b0ce1d01eae
Zhangpengyu <18746442232@163.com>
2021-06-04 19:09:09 +0800

    .

---
+++ 前端扫盲/javaScript/new.js
@@ -0,0 +1,7 @@
+function objectFactory(){
+    var obj = new Object(),
+    Constructor = [].shift.call(arguments);
+    obj.__proto__ = Constructor.prototype;
+    Constructor.apply(obj,arguments);
+    return obj;
+}
